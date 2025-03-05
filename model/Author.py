0eef5975e6ed561cb91a157c5b7a2f9ae4cd3888
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-03-05 23:36:18 +0200

    change the script to fetch books

--- model/Author.py
+++ model/Author.py
@@ -1,9 +1,9 @@
-from initializer.db_init import db
-
+from initializer.database import db
 
 class Author(db.Model):
     __tablename__ = "author"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    books = db.relationship('Book', backref='author', lazy=True)
+    open_library_key = db.Column(db.String(255), unique=True, nullable=False)  # authors/OL18319A
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    books = db.relationship("Book", backref="author", lazy=True)
