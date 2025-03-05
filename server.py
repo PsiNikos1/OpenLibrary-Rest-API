fcb657c3a730843dfa208c4d8154d61529346eb7
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-03-05 21:44:31 +0200

    create main

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from initializer.db_init import db, initialize_database
+from controllers.BookController import BookController
+import config  # Ensure config is explicitly imported
+from model.Book import Book
+
+app = Flask(__name__)
+
+# Load Configuration
+app.config.from_object("config")
+
+# Initialize Database (Only once)
+db.init_app(app)
+initialize_database(app)
+
+# Register Controllers
+book_controller = BookController(app, db)
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
+
+
