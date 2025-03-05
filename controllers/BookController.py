b9aec35c241a42fe25c7852d4340e6c78219910b
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-03-05 23:35:41 +0200

    add new endpoint to search book by title

--- controllers/BookController.py
+++ controllers/BookController.py
@@ -1,3 +1,4 @@
+import self
 from flask import Blueprint, jsonify, request
 from model.Book import Book
 from model.Author import Author
@@ -9,11 +10,13 @@ class BookController:
     def __init__(self, app, db):
         self.db = db
         self.book_bp = Blueprint("book_bp", __name__)
+        self.app = app
 
         self.book_bp.add_url_rule("/books", "get_books", self.get_books, methods=["GET"])
         self.book_bp.add_url_rule("/books/<int:book_id>", "get_book", self.get_book, methods=["GET"])
         self.book_bp.add_url_rule("/books", "add_book", self.add_book, methods=["POST"])
         self.book_bp.add_url_rule("/books/<int:book_id>", "delete_book", self.delete_book, methods=["DELETE"])
+        self.book_bp.add_url_rule("/books/<string:title>", "get_book_by_title", self.get_book_by_title, methods=["GET"])
 
         app.register_blueprint(self.book_bp)
 
@@ -24,6 +27,34 @@ class BookController:
     def get_book(self, book_id):
         book = Book.query.get_or_404(book_id)
         return jsonify({"title": book.title, "author": book.author.name, "work": book.work.title})
+
+    def get_book_by_title(self, title):
+        """ Get a book by title """
+        book = Book.query.filter(Book.title.ilike(f"%{title}%")).first()
+        if book:
+            return jsonify({
+                "id": book.id,
+                "title": book.title,
+                "author_id": book.author_id,
+                "work_id": book.work_id,
+                "publishers": book.publishers,
+                "number_of_pages": book.number_of_pages,
+                "isbn_10": book.isbn_10,
+                "edition_count": book.edition_count,
+                "subjects": book.subjects,
+                "publish_date": book.publish_date,
+                "cover_id": book.cover_id,
+                "open_library_key": book.open_library_key,
+                "first_publish_year": book.first_publish_year,
+                "languages": book.languages,
+                "lending_edition": book.lending_edition,
+                "lending_identifier": book.lending_identifier,
+                "project_gutenberg_ids": book.project_gutenberg_ids,
+                "librivox_ids": book.librivox_ids,
+                "ia_identifiers": book.ia_identifiers,
+                "public_scan": book.public_scan,
+            })
+        return jsonify({"error": "Book not found"}), 404
 
     def add_book(self):
         data = request.get_json()
