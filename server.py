88deda6abfd6b255b7f98e249c935cc7a238dde4
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-03-06 18:51:34 +0200

    register endpoints

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+
+from controllers.AuthorController import AuthorController
 from initializer.db_init import db, initialize_database
 from controllers.BookController import BookController
 import config  # Ensure config is explicitly imported
@@ -15,6 +17,7 @@ initialize_database(app)
 
 # Register Controllers
 book_controller = BookController(app, db)
+author_controller = AuthorController(app, db)
 
 if __name__ == "__main__":
 
